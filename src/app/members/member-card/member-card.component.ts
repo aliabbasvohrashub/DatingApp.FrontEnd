a05d5e9c10963b4c0098fa9950222a594e03da39
AliabbasVohra <aliabbasvohra@gmail.com>
2020-08-31 13:22:59 +0530

    intial commit

---
+++ src/app/members/member-card/member-card.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { User } from '../../_models/user';
+import { AuthService } from '../../_services/auth.service';
+import { UserService } from '../../_services/user.service';
+import { AlertifyService } from '../../_services/alertify.service';
+import { from, of, observable, Observable } from "rxjs";
+import { filter, map } from "rxjs/operators";
+
+@Component({
+  selector: 'app-member-card',
+  templateUrl: './member-card.component.html',
+  styleUrls: ['./member-card.component.css']
+})
+export class MemberCardComponent implements OnInit {
+  @Input() user: User;
+
+  constructor(private authService: AuthService, private userService: UserService, private alertify: AlertifyService) { }
+
+  ngOnInit() {
+
+    // from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    //   .pipe(
+    //     map(x => x * 2),
+    //     filter(x => x > 8)
+    //   )
+    //   .subscribe(x => console.log(x));
+
+    
+    // Observable.create([1, 2, 3, 4, 5])
+    //   .map(x => x * 2)
+    //   .filter(x => x > 5)
+    //   .subscribe(x => console.log(x));
+  }
+
+  sendLike(id: number) {
+    this.userService.sendLike(this.authService.decodedToken.nameid, id).subscribe(data => {
+      this.alertify.success('You have liked: ' + this.user.KnownAs);
+    }, error => {
+      this.alertify.error(error);
+    });
+  }
+
+}
